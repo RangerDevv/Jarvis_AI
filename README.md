127930a593c145fd411f6ac9159225e5815190fa
Rangerdevv <meerabdul2008@gmail.com>
2022-09-30 18:49:20 -0400

    jarvis is learning

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Jarvis_AI
 Jarvis from Iron Man.
+
+
+## List of commands that Jarvis knows currently
+- What is your name
+- quit
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from datetime import datetime
+from urllib import response
 import speech_recognition as sr
-import datetime
+import pyttsx3
 
 # Initialize recognizer class (for recognizing the speech)
 
 r = sr.Recognizer()
-
+def SpeakText(command):
+     
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
 
 # Reading Microphone as source
 # listening the speech and store in audio_text variable
@@ -13,11 +19,18 @@ while True:
     with sr.Microphone() as source:
         print("Talk")
         audio_text = r.listen(source)
+        resp = ""
         print(r.recognize_google(audio_text))
         if r.recognize_google(audio_text)=="what is your name":
-            print("My name is Jarvis, Your personal assistant")
-        elif  r.recognize_google(audio_text)=="quit":
+            resp = "My name is Jarvis, Your personal assistant. How can I help?"
+            SpeakText(resp)
+        if  r.recognize_google(audio_text)=="quit":
+            resp = "good day sir!"
+            SpeakText(resp)
             break
+        else:
+            resp = "sorry, I dont understand."
+            SpeakText(resp)
 # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
 
 
