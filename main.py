dda330fd5b9f7c2050d286f071b6a5e10aacaff3
Rangerdevv <meerabdul2008@gmail.com>
2022-10-02 17:07:52 -0400

    added a good wiki integration

--- main.py
+++ main.py
@@ -17,22 +17,31 @@ def SpeakText(command):
 # Reading Microphone as source
 # listening the speech and store in audio_text variable
 
+Questionwords = [
+    "who is",
+    "what is",
+    "what are"
+]
+
 while True:
+    SpeakText("Hello sir, how may I help u?")
     with sr.Microphone() as source:
         print("Talk")
         audio_text = r.listen(source)
-        print(r.recognize_google(audio_text))
+        # print(r.recognize_google(audio_text))
+        command = r.recognize_google(audio_text)
         resp = ""
-        if r.recognize_google(audio_text)=="what is your name":
+        if command=="what is your name":
             resp = "My name is Jarvis, Your personal assistant. How can I help?"
             SpeakText(resp)
-        if  r.recognize_google(audio_text)=="quit":
+        if  command=="quit":
             resp = "good day sir!"
             SpeakText(resp)
             break
-        if  r.recognize_google(audio_text)=="who is Albert Einstein":
-            search = wp.summary("Albert Einstein", sentences=4)
-            SpeakText(search)
+        if  "who is" or "what is" or "what are" in command:
+            search = command.replace( "who is" or "what is" or "what are" ," ")
+            result = wp.summary(search , 4)
+            SpeakText(result)
         else:
             resp = "sorry, I dont understand."
             SpeakText(resp)
